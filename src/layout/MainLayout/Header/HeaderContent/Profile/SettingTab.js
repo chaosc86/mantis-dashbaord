48bcb2530c1f86ff5739a946fb2ee3f4503e2f43
asd35084591 <k891051@gmail.com>
2023-08-12 22:39:40 +0800

    Update: Move 'Language' & 'Theme' Toggles to 'SettingTab'

--- src/layout/MainLayout/Header/HeaderContent/Profile/SettingTab.js
+++ src/layout/MainLayout/Header/HeaderContent/Profile/SettingTab.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { List, ListItemButton, ListItemIcon, ListItemText, ToggleButtonGroup, ToggleButton, Typography, Box } from '@mui/material';
+import { ThemeContext } from '../../../../../themes';
 
 // assets
-import { CommentOutlined, LockOutlined, QuestionCircleOutlined, UserOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import {
+  CommentOutlined,
+  LockOutlined,
+  QuestionCircleOutlined,
+  UserOutlined,
+  UnorderedListOutlined,
+  FontSizeOutlined,
+  AlertOutlined
+} from '@ant-design/icons';
 
 // ==============================|| HEADER PROFILE - SETTING TAB ||============================== //
 
@@ -17,10 +26,81 @@ const SettingTab = () => {
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [slot, setSlot] = useState('en');
+  const { mode, setMode } = useContext(ThemeContext);
+  const support_lang = { en: true, zh_TW: true };
+  const switchLang = (lang) => {
+    if (lang === null) return;
+    setSlot(lang);
+    localStorage.setItem('Language', lang);
+    i18n.changeLanguage(lang);
+  };
+  const switchTheme = (theme) => {
+    if (theme === null) return;
+    localStorage.setItem('theme', theme);
+    setMode(theme);
+  };
+  useEffect(() => {
+    if (
+      !localStorage.getItem('Language') ||
+      localStorage.getItem('Language') === undefined ||
+      support_lang?.[localStorage.getItem('Language')] === undefined
+    )
+      return switchLang(slot);
+    switchLang(localStorage.getItem('Language'));
+    if (!localStorage.getItem('theme') || localStorage.getItem('theme') === undefined) return switchTheme(mode);
+  }, []);
 
   return (
     <List component="nav" sx={{ p: 0, '& .MuiListItemIcon-root': { minWidth: 32, color: theme.palette.grey[500] } }}>
+      <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
+        <ListItemIcon>
+          <FontSizeOutlined />
+        </ListItemIcon>
+        <ListItemText primary={t('Language')} />
+        <ToggleButtonGroup
+          color="primary"
+          value={slot || 'en'}
+          exclusive
+          aria-label="text alignment"
+          onChange={(e, value) => {
+            switchLang(value);
+          }}
+        >
+          <ToggleButton size="small" value="en" aria-label="English">
+            <Typography noWrap>{t('En')}</Typography>
+          </ToggleButton>
+          <ToggleButton sx={{ mr: 1 }} size="small" value="zh_TW" aria-label="Standard Chinese">
+            <Box>
+              <Typography noWrap>{t('zh_TW')}</Typography>
+            </Box>
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </ListItemButton>
+      <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
+        <ListItemIcon>
+          <AlertOutlined />
+        </ListItemIcon>
+        <ListItemText primary={t('Theme')} />
+        <ToggleButtonGroup
+          color="primary"
+          value={localStorage.getItem('theme') || mode}
+          exclusive
+          aria-label="text alignment"
+          onChange={(e, value) => {
+            switchTheme(value);
+          }}
+        >
+          <ToggleButton size="small" value="light" aria-label="Light Theme">
+            <Typography noWrap>{t('Light')}</Typography>
+          </ToggleButton>
+
+          <ToggleButton sx={{ mr: 1 }} size="small" value="dark" aria-label="Dark Theme">
+            <Typography noWrap>{t('Dark')}</Typography>
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </ListItemButton>
       <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
         <ListItemIcon>
           <QuestionCircleOutlined />
--- src/layout/MainLayout/Header/HeaderContent/index.js
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -1,10 +1,8 @@
 // material-ui
-import { Box, IconButton, Link, useMediaQuery, ToggleButtonGroup, ToggleButton } from '@mui/material';
+import { Box, IconButton, Link, useMediaQuery } from '@mui/material';
 import { GithubOutlined } from '@ant-design/icons';
-import { useState, useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@mui/material/styles';
-import { ThemeContext } from '../../../../themes';
 
 // project import
 import Search from './Search';
@@ -16,74 +14,13 @@ import MobileSection from './MobileSection';
 const HeaderContent = () => {
   const matchesXs = useMediaQuery((theme) => theme.breakpoints.down('md'));
   const theme = useTheme();
-  const { t, i18n } = useTranslation();
-  const [slot, setSlot] = useState('en');
-  const { mode, setMode } = useContext(ThemeContext);
-  const support_lang = { en: true, zh_TW: true };
-  const switchLang = (lang) => {
-    if (lang === null) return;
-    setSlot(lang);
-    localStorage.setItem('Language', lang);
-    i18n.changeLanguage(lang);
-  };
-  const switchTheme = (theme) => {
-    if (theme === null) return;
-    localStorage.setItem('theme', theme);
-    setMode(theme);
-  };
-  useEffect(() => {
-    if (
-      !localStorage.getItem('Language') ||
-      localStorage.getItem('Language') === undefined ||
-      support_lang?.[localStorage.getItem('Language')] === undefined
-    )
-      return switchLang(slot);
-    switchLang(localStorage.getItem('Language'));
-    if (!localStorage.getItem('theme') || localStorage.getItem('theme') === undefined) return switchTheme(mode);
-  }, []);
+  const { t } = useTranslation();
 
   return (
     <>
       {!matchesXs && <Search />}
       {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
 
-      {
-        <ToggleButtonGroup
-          color="primary"
-          value={localStorage.getItem('theme') || mode}
-          exclusive
-          aria-label="text alignment"
-          onChange={(e, value) => {
-            switchTheme(value);
-          }}
-        >
-          <ToggleButton size="small" value="light" aria-label="Light Theme">
-            Light
-          </ToggleButton>
-
-          <ToggleButton sx={{ mr: 1 }} size="small" value="dark" aria-label="Dark Theme">
-            Dark
-          </ToggleButton>
-        </ToggleButtonGroup>
-      }
-      {
-        <ToggleButtonGroup
-          color="primary"
-          value={slot || 'en'}
-          exclusive
-          aria-label="text alignment"
-          onChange={(e, value) => {
-            switchLang(value);
-          }}
-        >
-          <ToggleButton size="small" value="en" aria-label="English">
-            En
-          </ToggleButton>
-          <ToggleButton sx={{ mr: 1 }} size="small" value="zh_TW" aria-label="Standard Chinese">
-            zh_TW
-          </ToggleButton>
-        </ToggleButtonGroup>
-      }
       <IconButton
         component={Link}
         href="https://github.com/chaosc86/mantis-dashbaord"
